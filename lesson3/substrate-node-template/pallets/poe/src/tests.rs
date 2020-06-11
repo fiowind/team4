3a6b9869e2c38d84fe8b1fc7aab84fbf10aa9a33
ylic <ylic@263.net>
2020-06-11 20:55:42 +0800

    HW03

--- lesson3/substrate-node-template/pallets/poe/src/tests.rs
+++ lesson3/substrate-node-template/pallets/poe/src/tests.rs
@@ -1,11 +1,11 @@
 // Tests to be written here
-//  shell command: cargo test -p pallets-poe
+//  shell command: cargo test -p pallet-poe
 
 
 use super::*;
 use crate::{Error,mock::*};
 
-use frame_support::{assert_ok,assert_noop};
+use frame_support::{assert_ok,assert_noop,assert_ne};
 
 //teset cases for create_claim
 #[test]
@@ -55,3 +55,50 @@ fn create_claim_failed_when_proof_already_exist(){
 			);
 	})
 }
+
+
+//teset cases for remove_claim
+#[test]  
+fn remove_claim_failed_when_proof_not_exist(){
+	new_test_ext().execute_with(||{
+		let claim = vec![0,1,2,3];
+		assert_noop!(
+			PoeModule::remove_claim(Origin::signed(1),claim.clone()),
+			Error::<Test>::ProofNotExist
+			);
+	})
+}
+
+fn remove_claim_failed_when_not_have_permission(){
+	new_test_ext().execute_with(||{
+		let claim = vec![0,1,2,3];
+
+		let _ = PoeModule::create_claim(Origin::signed(1),claim.clone());
+
+		assert_noop!(
+			PoeModule::remove_claim(Origin::signed(2),claim.clone()),
+			Error::<Test>::NotHavePermission
+			);
+	})
+}
+
+
+fn remove_claim_work(){
+	new_test_ext().execute_with(||{
+		let claim = vec![0,1,2,3];
+
+		let _ = PoeModule::create_claim(Origin::signed(1),claim.clone());
+
+
+		assert_ok!(PoeModule::create_claim(Origin::signed(1),claim.clone()));
+
+
+		assert_ne!(Proofs::<Test>::get(&claim),(1,system::Module::<Test>::block_number()));
+
+
+	})
+}
+
+
+
+
