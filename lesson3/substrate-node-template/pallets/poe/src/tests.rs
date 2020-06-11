cbefbbb88ac35cc9e9256d5b0ebcf1d51ed87937
ylic <ylic@263.net>
2020-06-11 19:30:30 +0800

    HW03

--- lesson3/substrate-node-template/pallets/poe/src/tests.rs
+++ lesson3/substrate-node-template/pallets/poe/src/tests.rs
@@ -9,7 +9,7 @@ use frame_support::{assert_ok,assert_noop};
 #[test]
 fn create_claim_works(){
 	new_test_ext().execute_with(||{
-		let claim = vec![0,1];
+		let claim = vec![0,1,3,4];
 		assert_ok!(PoeModule::create_claim(Origin::signed(1),claim.clone()));
 		assert_eq!(Proofs::<Test>::get(&claim),(1,system::Module::<Test>::block_number()));
 	})
