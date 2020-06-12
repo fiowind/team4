541e74052d08384483b816d92187d839d74ee5a5
ylic <ylic@263.net>
2020-06-12 19:05:05 +0800

    Add HomeWork Lesson 3

--- lesson3/substrate-front-end-template/src/Events.js
+++ lesson3/substrate-front-end-template/src/Events.js
@@ -57,7 +57,7 @@ function Main (props) {
         icon='erase'
         onClick={ _ => setEventFeed([]) }
       />
-      <Feed style={{ clear: 'both', overflow: 'auto', maxHeight: 600 }} events={eventFeed} />
+      <Feed style={{ clear: 'both', overflow: 'auto', height: 600 }} events={eventFeed} />
     </Grid.Column>
   );
 }
