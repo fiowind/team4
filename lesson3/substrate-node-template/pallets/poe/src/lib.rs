ffdec311f30a0c0e94984aecf50641ef97c82785
liaoshifu <lshoo36@gmail.com>
2020-06-12 22:47:03 +0800

    第三课作业第二题

--- lesson3/substrate-node-template/pallets/poe/src/lib.rs
+++ lesson3/substrate-node-template/pallets/poe/src/lib.rs
@@ -42,6 +42,7 @@ decl_event!(
 	pub enum Event<T> where AccountId = <T as system::Trait>::AccountId {
 		ClaimCreated(AccountId, Vec<u8>),
 		ClaimRevoked(AccountId, Vec<u8>),
+		ClaimTransfered(AccountId, Vec<u8>, AccountId),
 	}
 );
 
@@ -114,7 +115,9 @@ decl_module! {
 
 			let dest = T::Lookup::lookup(dest)?;
 
-			Proofs::<T>::insert(&claim, (dest, system::Module::<T>::block_number()));
+			Proofs::<T>::insert(&claim, (&dest, system::Module::<T>::block_number()));
+
+			Self::deposit_event(RawEvent::ClaimTransfered(sender, claim, dest));
 
 			Ok(())
 		}
